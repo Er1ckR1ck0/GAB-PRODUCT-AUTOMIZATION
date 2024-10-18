e502147717cc93ead9c5a4699a5ceba95020156e
Er1ckR1ck0 <132528571+Er1ckR1ck0@users.noreply.github.com>
2024-10-18 04:05:02 +0300

    fix: structure Lock

--- main.py
+++ main.py
@@ -27,6 +27,4 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=8000,
         reload=True,
-        log_level="info",  # Уровень логирования
-        access_log=True  # Включение логирования доступа
     )
